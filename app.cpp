f55f58d8edf04cbf6c1eddfc2c0a13940fe57b95
ketvok <115495534+ketvok@users.noreply.github.com>
2022-10-24 13:57:07 +0200

    Add files via upload

--- app.cpp
+++ app.cpp
@@ -1,5 +1,6 @@
 #include "app.h"
 #include <format>
+#include <sstream>
 
 
 namespace vsite::oop::v1
@@ -8,9 +9,41 @@ namespace vsite::oop::v1
 * function implementations
 */
 	std::string to_hex(int n) {
+		return std::format("{:X}", n);
+	}
+
+	std::string to_exp(double n) {
+		return std::format("{:.2e}", n);
+	}
+
+	/*
+		Ovo je moj neki teži naèin bez formata...
+		
+		std::string to_hex(int n) {
 		char hex_string[20];
 		sprintf_s(hex_string, "%X", n);
 		return hex_string;
+	}
+	*/
+
+
+	void mult_table(int n, std::stringstream& ss) {
+		if (n <= 0 || n > 20) {  // tablica radi samo za brojeve od 0 do 20
+			ss << "";
+		}
+		else {
+			ss << 0;  // ispis prvog reda u tablici
+			for (int i = 1; i < n; i++)
+				ss << std::format("{:4}", i);
+			ss << std::format("{:4}", n) << "\n";
+
+			for (int i = 1; i <= n; i++) {
+				ss << i; // u svakom redu prvo ispisi i,...
+				for (int j = 1; j < n; j++)  // ...a dalje je mnozenje i * j...
+					ss << std::format("{:4}", i * j);
+				ss << std::format("{:4}", i * n) << "\n";  // ...s tim da na zadnjem (j==n) još i preðem u novi red
+			}
+		}
 	}
 
 } // namespace
--- app.h
+++ app.h
@@ -7,6 +7,9 @@ namespace vsite::oop::v1
 /*
 * function declarations
 */
-	std::string to_hex(int a);
+	std::string to_hex(int n);
 
+	std::string to_exp(double n);
+
+	void mult_table(int n, std::stringstream& ss);
 }
