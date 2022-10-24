408245d5d613a434d1d954ccf8cce34c976dd31c
ketvok <115495534+ketvok@users.noreply.github.com>
2022-10-24 15:32:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "app.h"
+#include <format>
+#include <sstream>
+
+
+// input one integer
+// write its hex representation to std::cout (using vsite::oop::v1::to_hex)
+// write multiplication table up to this number to std::cout (using vsite::oop::v1::mult_table)
+
+
+void moja_funkcija_v01(double n) {
+	std::cout << "Hexadecimalna vrijednost:\n";
+	std::cout << std::format ("{}{}",vsite::oop::v1::to_hex(n), "\n");
+
+	std::cout << "Tablica mnozenja do zadanog broja:\n";
+	std::stringstream moj_stream;
+	vsite::oop::v1::mult_table(n, moj_stream);
+	std::cout << moj_stream.str();
+}
+
+
+int main(){
+	double n;
+	std::cout << "Unesite cijeli broj:\n";
+	std::cin >> n;
+
+	moja_funkcija_v01(n);
+}
