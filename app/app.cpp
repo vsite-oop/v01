9d8f14c8992313d626f0a65d6b01b0cceffc19e0
Filip Les <ketvok@gmail.com>
2022-10-25 11:40:22 +0200

    Svi testovi prolaze i funkcija radi.

--- app/app.cpp
+++ app/app.cpp
@@ -29,17 +29,17 @@ namespace vsite::oop::v1
 
 
 	void mult_table(int n, std::stringstream& ss) {
-		if (n <= 0 || n > 20) {  // table works only for numbers from 0 to 20
+		if (n <= 0 || n > 20) {  // Table works only for numbers from 0 to 20
 			ss << "";
 		}
 		else {
-			ss << 0;  // writing first table row
+			ss << 0;  // Writing first table row
 			for (int i = 1; i < n; i++)
 				ss << std::format("{:4}", i);
 			ss << std::format("{:4}", n) << "\n";
 
 			for (int i = 1; i <= n; i++) {
-				ss << i; // in each row first write "i",...
+				ss << i;  // In each row first write "i",...
 				for (int j = 1; j < n; j++)  // ...else is multiplication i * j...
 					ss << std::format("{:4}", i * j);
 				ss << std::format("{:4}", i * n) << "\n";  // ...additionally, in the last one (j==n) move to the next row.
