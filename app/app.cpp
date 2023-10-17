36ea5dc40086f5ed1e4c78a054dbaca96b1f8b17
Josip Grabušić <jograbus@vsite.hr>
2023-10-17 19:50:06 +0200

    Ispravljeni app.cpp

--- app/app.cpp
+++ app/app.cpp
@@ -9,7 +9,7 @@ namespace vsite::oop::v1
         return std::format("{:X}", broj);
     }
 
-    
+
     std::string to_exp(double broj) {
         return std::format("{:.2e}", broj);
     }
@@ -21,29 +21,17 @@ namespace vsite::oop::v1
 
         for (int i = 0; i < broj + 1; i++) {
             for (int j = 0; j < broj + 1; j++) {
-                if (i == 0 && j == 0) {
-                    ss << j;
+                if (i == 0 || j == 0) {
+                    ss << (i + j);
+                    if (j != broj) ss << std::setw(4);
                 }
-                else 
-                    if (j == 0) {
-                    ss << i;
-                    }
-                     else {
-                        if (i > 0) {
-                            std::string znamenka = std::to_string(j * i);
-                            ss << std::setw(4) << znamenka;
+                else {
+                    ss << (i * j);
+                    if (j != broj) ss << std::setw(4);
+                }
+            }ss << "\n";
 
-                        }
-                        else {
-                            std::string znamenka = std::to_string(j * 1);
-                            ss << std::setw(4) << znamenka;
-
-                        }
-                    
-                     }
-            }
-            ss << "\n";
+                
         }
     }
-
 } // namespace
