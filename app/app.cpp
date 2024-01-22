e9b2e1dc406ab906d988c75ba18aedb9db75e094
Fran <fran.habijanic@gmail.com>
2024-01-22 19:35:02 +0100

    vj1

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,41 @@
 #include "app.h"
+#include <ostream>
+#include <iomanip>
 #include <format>
 
 namespace vsite::oop::v1
 {
-/*
-* function implementations
-*/
+	std::string to_hex(int input) {
+		std::string out = std::format("{:0X}", input);
+		return out;
+	}
 
-} // namespace
+	std::string to_exp(double input) {
+		std::string out = std::format("{:1.2e}", input);
+		return out;
+	}
+
+	void mult_table(int n, std::ostream& ss) {
+		if (n == 0 || n > 20) {
+			ss << "";
+			return;
+		}
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= n; j++) {
+				if (!i && !j)
+					ss << 0;
+				else if (!i)
+					ss << std::setw(4) << j;
+				else if (!j && i != 0)
+					ss << i;
+				else {
+					ss << std::setw(4) << i * j;
+				}
+			}
+			ss << "\n";
+		}
+
+	}
+
+} 
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,22 @@
 #include "app.h"
+#include <iostream>
 
+
+using namespace vsite::oop::v1;
 // input one integer
 // write its hex representation to std::cout (using vsite::oop::v1::to_hex)
 // write multiplication table up to this number to std::cout (using vsite::oop::v1::mult_table)
 
-int main()
-{
+int main() {
+	int x;
+	while (true) {
+		std::cout << "Unesite cijeli broj:";
+		std::cin >> x;
+		if (x > 0 && x != 0)
+			break;
+	}
+
+	std::cout << to_hex(x);
+	std::cout << "\n";
+	mult_table(x, std::cout);
 }
